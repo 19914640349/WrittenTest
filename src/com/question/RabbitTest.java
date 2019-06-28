e7736e69cdd1278c71c1f9266829410dd0f12cf7
xc <123@qq.com>
2019-06-28 08:43:50 +0800

    更新你好语句3

--- src/com/question/RabbitTest.java
+++ src/com/question/RabbitTest.java
@@ -4,8 +4,8 @@ public class RabbitTest {
     public static void main(String[] args) {
         System.out.println("玩兔子");
         System.out.println("你好");
-        System.out.println("完犊子的兔子");
-        System.out.println("完犊子");
+        System.out.println("兔子");
+
     }
 
 }
