c4413f5155cb71dac32a48a87972f2173d73d670
a934940321 <934940321@qq.com>
2019-09-15 20:53:51 +0800

    Update and rename src/com/question/RabbitTest.java to SRC/COM/问题/RabbitTest.java

--- SRC/COM/问题/RabbitTest.java
+++ SRC/COM/问题/RabbitTest.java
@@ -2,9 +2,9 @@ package com.question;
 
 public class RabbitTest {
     public static void main(String[] args) {
-        System.out.println("玩兔子");
+        System.out.println("大兔子");
         System.out.println("你好");
-        System.out.println("兔子");
+        System.out.println("小兔子");
         System.out.println("hello");
     }
 
