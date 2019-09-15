f697c2b200fe060659109e6aff4f1ac1e002af33
a934940321 <934940321@qq.com>
2019-09-15 21:26:32 +0800

    Update RabbitTest.java

--- SRC/COM/问题/RabbitTest.java
+++ SRC/COM/问题/RabbitTest.java
@@ -6,7 +6,7 @@ public class RabbitTest {
         System.out.println("你好");
         System.out.println("小兔子");
         System.out.println("hello2");
-        System.out.println("你好，我的分支2");
+      
     }
 
 }
